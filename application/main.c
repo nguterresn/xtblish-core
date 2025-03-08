ce940151f23ce49cfb7095e6ce210ef745252fa1
Nuno Guterres Nogueira <guterresnogueira@gmail.com>
2025-03-08 15:33:20 +0100

    remaining heap is really small - gotta fix that later.

--- application/main.c
+++ application/main.c
@@ -18,7 +18,6 @@
 	                K_NO_WAIT)
 
 static K_THREAD_STACK_DEFINE(app_stack, 8192);
-// static K_THREAD_STACK_DEFINE(http_stack, 8192);
 
 static struct k_thread _app_thread;
 
@@ -43,7 +42,4 @@ int main(void)
 
 	N_THREAD(_app_thread, app_stack, app_thread);
 	k_thread_name_set(&_app_thread, "app_thread");
-
-	// N_THREAD(_http_thread, http_stack, http_thread);
-	// k_thread_name_set(&_http_thread, "http_thread");
 }
